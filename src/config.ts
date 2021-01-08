1a490b49aa1e0d88cfab6329bea11c840829c7d3
Nazarii <nazarii.shvets@gmail.com>
2021-01-09 01:22:03 +0200

    .env

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,12 @@
+import dotenv from 'dotenv';
+
+const result: dotenv.DotenvConfigOutput = dotenv.config();
+if (result.error) throw new Error('DoteEnv error');
+
 export const PORT: string | number = process.env.PORT || 3000;
-export const dbPassword: string = 'Divine666';
-export const dbName: string = 'task-to-intership';
-export const dbUser: string = 'postgres';
-export const dbTeachersTableName: string = 'teachers';
-export const dbLessonsTableName: string = 'lessons';
-export const dbClassroomTableName: string = 'classrooms';
+export const dbPassword: string = process.env.DB_PASSWORD!;
+export const dbName: string = process.env.DB_NAME!;
+export const dbUser: string = process.env.DB_USER!;
+export const dbTeachersTableName: string = process.env.DB_TEACHERS_TABLE_NAME!;
+export const dbLessonsTableName: string = process.env.DB_LESSONS_TABLE_NAME!;
+export const dbClassroomTableName: string = process.env.DB_CLASSROOMS_TABLE_NAME!;
